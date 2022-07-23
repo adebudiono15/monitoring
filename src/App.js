299878c0029a9c950a560c780d73c7cf6900ecc0
Budiono <budi@Budis-MacBook-Pro.local>
2022-07-23 22:15:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,77 @@
-import * as React from 'react';
+import React, {Component, componentDidMount} from 'react';
 import 'react-native-gesture-handler';
 import {Monitoring1, Monitoring2} from './pages';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import PushNotification from 'react-native-push-notification';
+import Firebase from '@react-native-firebase/app';
 
 const Drawer = createDrawerNavigator();
-function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Monitoring1">
-        <Drawer.Screen name="Monitoring1" component={Monitoring1} />
-        <Drawer.Screen name="Monitoring2" component={Monitoring2} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+
+class App extends Component {
+  componentDidMount() {
+    Firebase.initializeApp(this);
+    PushNotification.configure({
+      // (optional) Called when Token is generated (iOS and Android)
+      onRegister: function (token) {
+        console.log('TOKEN:', token);
+      },
+
+      // (required) Called when a remote is received or opened, or local notification is opened
+      onNotification: function (notification) {
+        console.log('NOTIFICATION:', notification);
+
+        // process the notification
+
+        // (required) Called when a remote is received or opened, or local notification is opened
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+
+      // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+      onAction: function (notification) {
+        console.log('ACTION:', notification.action);
+        console.log('NOTIFICATION:', notification);
+
+        // process the action
+      },
+
+      // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+      onRegistrationError: function (err) {
+        console.error(err.message, err);
+      },
+
+      // IOS ONLY (optional): default: all - Permissions to register.
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true,
+      },
+
+      // Should the initial notification be popped automatically
+      // default: true
+      popInitialNotification: true,
+
+      /**
+       * (optional) default: true
+       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+       * - if you are not using remote notification or do not have Firebase installed, use this:
+       *     requestPermissions: Platform.OS === 'ios'
+       */
+      requestPermissions: true,
+    });
+  }
+  render() {
+    return (
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Monitoring1">
+          <Drawer.Screen name="Monitoring1" component={Monitoring1} />
+          <Drawer.Screen name="Monitoring2" component={Monitoring2} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 export default App;
